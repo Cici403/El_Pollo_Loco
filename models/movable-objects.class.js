8979709a6705cb9bd67da85071b6f6bc045ed936
Cici403 <christian.schreter@gmail.com>
2025-02-19 15:14:34 +0100

    add the witdh and height in the thml canvas tag, problem form the show pepe ist resolved

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -1,8 +1,15 @@
 class MovableObject{
     x = 120;
-    y = 400;
+    y = 150;
     img;
+    width = 100;
+    height = 100;
 
+
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
     moveRight(){
 
     }
