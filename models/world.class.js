34ec13be175e1c802ff0bf7f4c43a27e270d6d4a
Cici403 <christian.schreter@gmail.com>
2025-02-20 21:31:03 +0100

    aded the bakground images

--- models/world.class.js
+++ models/world.class.js
@@ -2,7 +2,12 @@ class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud()];
-  backgroundObjects = [new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0, 100)];
+  backgroundObjects = [
+    new BackgroundObject("img/5_background/layers/air.png", 0),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
+  ];
   canvas;
   ctx;
 
@@ -15,10 +20,10 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.addToMap(this.character);
     this.addObjectsToMap(this.backgroundObjects);
-    this.addObjectsToMap(this.enemies);
     this.addObjectsToMap(this.clouds);
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.enemies);
 
     let self = this;
     requestAnimationFrame(function () {
