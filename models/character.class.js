868bc1ea3f4dbb7975569e05a6677d3735fdd55c
Cici403 <christian.schreter@gmail.com>
2025-02-20 21:35:34 +0100

    changed the sizes from the character andthe enemies

--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,7 @@
 class Character extends MovableObject {
+  height = 280;
+  width = 120;
+  y = 145;
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
   }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,7 @@
 class Chicken extends MovableObject {
-  y = 350;
-  height = 80;
-  width = 80;
+  y = 360;
+  height = 60;
+  width = 60;
   constructor() {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.x = 200 + Math.random() * 500;
