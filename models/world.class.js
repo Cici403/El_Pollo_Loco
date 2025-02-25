3fb627732dade55836ca8c5e3186398051bc28f0
Cici403 <christian.schreter@gmail.com>
2025-02-25 21:19:48 +0100

    added the move right and sflip image functions

--- models/world.class.js
+++ models/world.class.js
@@ -26,7 +26,6 @@ class World {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
     this.addObjectsToMap(this.backgroundObjects);
     this.addObjectsToMap(this.clouds);
     this.addToMap(this.character);
@@ -45,16 +44,24 @@ class World {
   }
 
   addToMap(mo) {
-    if(mo.otherDirection){
-      this.ctx.save();
-      this.ctx.translate(mo.width, 0);
-      this.ctx.scale(-1, 1);
-      mo.x = mo.x *-1;
+    if (mo.otherDirection) {
+      this.flipImage(mo);
     }
     this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
-    if(mo.otherDirection){
-      mo.x = mo.x * -1;
-      this.ctx.restore();
+    if (mo.otherDirection) {
+      this.flipImageBack(mo);
     }
+  }
+
+  flipImage(mo) {
+    this.ctx.save();
+    this.ctx.translate(mo.width, 0);
+    this.ctx.scale(-1, 1);
+    mo.x = mo.x * -1;
+  }
+
+  flipImageBack(mo) {
+    mo.x = mo.x * -1;
+    this.ctx.restore();
   }
 }
