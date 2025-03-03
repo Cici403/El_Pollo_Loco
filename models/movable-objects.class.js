7139b2cbfe63ec567d0c430c557f6d5928dde419
Cici403 <christian.schreter@gmail.com>
2025-03-03 19:57:17 +0100

    added the stastus bar class

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -11,6 +11,7 @@ class MovableObject {
   speedY = 0;
   acceleration = 2.5;
   energy = 100;
+  lastHit = 0;
 
   applyGravity() {
     setInterval(() => {
@@ -76,15 +77,21 @@ class MovableObject {
   }
 
   hit() {
-    console.log("hit", this.energy);
-
     this.energy -= 5;
     if (this.energy < 0) {
       this.energy = 0;
+    } else {
+      this.lastHit = new Date().getTime();
     }
   }
 
   isDead() {
     return this.energy == 0;
+  }
+
+  isHurt(){
+    let timepassed = new Date().getTime() - this.lastHit;
+    timepassed = timepassed / 1000;
+    return timepassed < 0.75;
   }
 }
